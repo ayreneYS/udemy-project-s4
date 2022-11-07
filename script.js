5411255b2605cd47482eb43bb0cbb22f39458b03
Ayrene <112762086+ayreneYS@users.noreply.github.com>
2022-11-07 22:05:06 +0800

    Bankist App

--- script.js
+++ script.js
@@ -67,118 +67,19 @@ const displayMovements = function (movements) {
   movements.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
 
-    const html = `<div class="movements__row">
-    <div class="movements__type movements__type--${type}">${i + 1}</div>
-    <div class="movements__value">${mov}</div>
-  </div>`;
+    const html = `
+      <div class="movements__row">
+        <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+        <div class="movements__value">${mov}€</div>
+      </div>
+    `;
+
     containerMovements.insertAdjacentHTML('afterbegin', html);
   });
 };
 displayMovements(account1.movements);
 
-/*
 
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// LECTURES
 
-// const currencies = new Map([
-//   ['USD', 'United States dollar'],
-//   ['EUR', 'Euro'],
-//   ['GBP', 'Pound sterling'],
-// ]);
-
-// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
-/////////////////////////////////////////////////
-
-//  Simple Array Methods //
-
-let arr = ['a', 'b', 'c', 'd', 'e'];
-
-// slice
-console.log(arr.slice(2));
-
-// splice
-arr.splice(-1);
-console.log(arr);
-
-// reverse
-arr = ['a', 'b', 'c', 'd', 'e'];
-const arr2 = ['j', 'i', 'h', 'g', 'f'];
-console.log(arr2.reverse());
-
-// concat
-const letters = arr.concat(arr2);
-console.log(letters);
-console.log([...arr, ...arr2]);
-
-// join
-console.log(letters.join(' - '));
-
-/////////////////////////////////////////////////
-
-//  The New at Method //
-
-const arr = [23, 11, 64];
-console.log(arr[0]);
-console.log(arr.flat(0));
-
-// getting last array element
-console.log(arr[arr.length - 1]);
-console.log(arr.slice(-1)[0]);
-console.log(arr.at(-1));
-
-console.log('ays'.at(0));
-console.log('ays'.at(-1));
-
-/////////////////////////////////////////////////
-
-//  Looping Arrays: forEach //
-
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
-// for of (can break loop)
-// for (const movement of movements) {
-for (const [i, movement] of movements.entries()) {
-  if (movement > 0) {
-    console.log(`Movement ${i + 1}: You deposited ${movement}`);
-  } else {
-    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
-  }
-}
-
-console.log('---- forEach ----'); // doesnt break the loop
-movements.forEach(function (mov, i, arr) {
-  if (mov > 0) {
-    console.log(`Movement ${i + 1}: You deposited ${mov}`);
-  } else {
-    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`);
-  }
-});
-// 0: function(200)
-// 1: function(450)
-// 2: function(400)
-// ...
-
-/////////////////////////////////////////////////
-
-// forEach With Maps and Sets //
-
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
-
-currencies.forEach(function (value, key, map) {
-  console.log(`${key}: ${value}`);
-});
-
-// set
-const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
-console.log(currenciesUnique);
-currenciesUnique.forEach(function (value, _, map) {
-  console.log(`${value}: ${value}`);
-});
-*/
